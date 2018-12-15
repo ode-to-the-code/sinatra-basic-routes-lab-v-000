e83caa9ff6c615fc2ebd350dcfeb763f1ac76014
Jad Abdallah <justcallmethis@gmail.com>
2018-12-15 18:33:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Jad"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Austin"
+  end 
+  
+  get '/favorite-song' do
+    "My favorite song is changes by tupac mfing shakur"
+  end 
+  
+  
 end
